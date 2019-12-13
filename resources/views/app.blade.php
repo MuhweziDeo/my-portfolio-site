2fa38d3d6c85024775055cc495e2dbe36809d918
muhwezideo <aggrey256@gmail.com>
2019-12-13 16:33:47 +0300

    add procfile for heroku

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,6 @@
     <body>
     <div id="app">
     </div>
-
-    <script src="{{asset('js/app.js')}}"> </script> 
+    <script src="{{asset('js/app.js', true)}}" defer> </script> 
     </body>
 </html>
