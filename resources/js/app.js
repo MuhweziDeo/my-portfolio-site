bf1b7eb7d5c1ae5408cf59e0434ab8eff46f3bc6
muhwezideo <aggrey256@gmail.com>
2019-12-13 17:49:26 +0300

    add vue material

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,12 @@ import Vue from "vue";
 import Vuetify from "vuetify";
 import router from "./routes";
 import App from "./components/App";
+import VueMaterial from "vue-material";
+import 'vue-material/dist/vue-material.min.css';
+
 
 Vue.use(Vuetify);
+Vue.use(VueMaterial);
 
 const app = new Vue({
     el: '#app',
