7bc3689a2fabcb42bffbbd047f913a9480dbee76
muhwezideo <aggrey256@gmail.com>
2020-01-04 23:48:18 +0300

    fix responsiveness

--- resources/js/components/views/Header.vue
+++ resources/js/components/views/Header.vue
@@ -8,9 +8,9 @@
                 <h3 class="text--2">IAM MUHWEZI DEO</h3>
             </div>
             <div class="header job">
-                <h1>JAVASCRIPT Engineer</h1>
+                <h1>FullStack Engineer</h1>
             </div>
-            <h4 class="header intro"> Freelance FullStack Developer & Love to Play Games</h4>
+            <h4 class="header intro">I  Love to Play Games and Code</h4>
             <div class="header__call-to-action">
                 <Button btn-class="header hire-me" text="Hire Me" />
                 <Button textColor="#f67280" btn-class="header__button" buttonColor="#f67280" text="Get CV" />
@@ -18,7 +18,7 @@
         </div>
 
         <div class="header__right">
-            <img class="img" src="../../img/placeholder.png" alt="No Image" />
+            <img class="img" src="../../img/39124174.png" alt="No Image" />
             <div class="social md-elevation-4">
            <md-icon class="fa fa-facebook"></md-icon>
            <md-icon class="fa fa-github"></md-icon>
@@ -32,7 +32,7 @@
 
 <script>
     import Button from "../shared/Button";
-    
+
     export default {
         components: {
             Button
@@ -42,86 +42,116 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "resources/js/css/_variables.scss";
+
     .header {
-        display: flex;
-        align-items: center;
-        justify-items: flex-end;
-        transform: translateY(30%);
-
-        .text {
-            display: flex;
-            justify-content: flex-start;
-            word-spacing: 0.2rem;
-
-            &--1 {
-                color: #f67280;
-                margin-right: .4rem;
-            }
-
+        &__button {
+            border: .1rem solid #f67280;
+            color: red;
+            background: white;
+            width: 100%;
         }
-
         .intro {
             font-weight: 300;
             color: grey;
         }
 
-        .job {
-            font-size: .7rem;
-            text-transform: uppercase;
-        }
-
-        .hire-me {
-            width: 80%;
-            margin-top: -.6rem;
-            margin-left: -.1rem;
-        }
-
-        &__left {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            align-self: center;
-        }
-
-        &__call-to-action {
-            display: flex;
-            justify-content: flex-start;
-        }
-
-        &__button {
-            border: .1rem solid #f67280;
-            color: #f67280;
-            background: white;
-        }
-
-        &__right {
-
-            .img {
-                width: 100%;
-                padding-left: 10rem;
-            }
-
-        }
         .social {
             display: flex;
             justify-content: space-between;
             padding: 1rem;
             border-radius: 45px;
-            width: 45%;
-            transform: translateX(38rem) translateY(-5rem);
+            width: 100%;
             box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
             cursor: pointer;
-        
+            margin-top: 1rem;
         }
 
-        .fa {
-            cursor: pointer;
-              color: #f67280;
-        }
-        .fa.fa-facebook {
-            border-radius: 45px;
+        .hire-me {
+            width: 100%;
+            margin-top: -.6rem;
+            margin-left: -.2rem;
         }
 
     }
+
+    @include breakpoint(desktop) {
+        .header {
+            display: flex;
+            align-items: center;
+            justify-items: flex-end;
+            transform: translateY(30%);
+
+            .text {
+                display: flex;
+                justify-content: flex-start;
+                word-spacing: 0.2rem;
+
+                &--1 {
+                    color: #f67280;
+                    margin-right: .4rem;
+                }
+
+            }
+
+            .intro {
+                font-weight: 300;
+                color: grey;
+            }
+
+            .job {
+                font-size: .7rem;
+                text-transform: uppercase;
+            }
+
+            .hire-me {
+                width: 100%;
+                margin-top: -.6rem;
+                margin-left: -.1rem;
+            }
+
+            &__left {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                align-self: center;
+            }
+
+            &__call-to-action {
+                display: flex;
+                justify-content: flex-start;
+            }
+
+            &__right {
+
+                .img {
+                    width: 100%;
+                    padding-left: 10rem;
+                }
+
+            }
+            .social {
+                display: flex;
+                justify-content: space-between;
+                padding: 1rem;
+                border-radius: 45px;
+                width: 45%;
+                transform: translateX(38rem) translateY(-5rem);
+                box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+                cursor: pointer;
+
+            }
+
+            .fa {
+                cursor: pointer;
+                color: #f67280;
+            }
+            .fa.fa-facebook {
+                border-radius: 45px;
+            }
+
+        }
+    }
+
 
 </style>
